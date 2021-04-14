802933e6f05e03205e897ea979f45281ed150954
expert40rus <vladik.monstor@mail.ru>
2021-04-14 22:01:42 +0300

    Добавил рандому

--- kinopoisk.py
+++ kinopoisk.py
@@ -6,4 +6,7 @@ kinopoisk = KP(token='9736285b-b7d0-4a40-9f9e-7cfb09354984')
 
 def get_cinema(genre):
     search_result = kinopoisk.search(genre)
-    return search_result[random.randrange(0, 4)].ru_name
+    if genre == 'Аниме':
+        return search_result[random.randrange(0, 4)].ru_name
+    else:
+        return search_result[random.randrange(0, 6)].ru_name
--- kinopoisk_api.py
+++ kinopoisk_api.py
@@ -87,7 +87,7 @@ class KP:
             data['imdb_rate'] = imdb_rate
             return FILM(data)
 
-        for _ in range(10):
+        for _ in range(100):
             try:
                 request = requests.get(self.API + 'films/' + str(film_id), headers=self.headers)
                 request_json = json.loads(request.text)
@@ -114,7 +114,7 @@ class KP:
                 continue
 
     def search(self, query):
-        for _ in range(10):
+        for _ in range(100):
             try:
                 request = requests.get(self.API + 'films/search-by-keyword', headers=self.headers,
                                        params={"keyword": query, "page": 1})
@@ -131,7 +131,7 @@ class KP:
                 continue
 
     def top500(self):
-        for _ in range(10):
+        for _ in range(100):
             try:
                 request = requests.get(self.API + 'films/top?type=BEST_FILMS_LIST&page=1&listId=1',
                                        headers=self.headers
